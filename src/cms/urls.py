e16dd12f9d57770dd413320ca1f2661eae7d7af2
andres-root <andres.root.coder@gmail.com>
2015-01-05 22:42:49 -0500

    added list view and url

--- src/cms/urls.py
+++ src/cms/urls.py
@@ -4,4 +4,10 @@ from django.conf.urls import *
 urlpatterns = patterns(
     'cms.views',
     url(r'^$', 'index', name='index'),
+    url(r'^(?P<year>\d{4})/$', 'list', name="year_archive"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'list', name="month_archive"),
+    url(r'^tag/(?P<tag>[\w-]+)/$', 'list', name='tag_archive'),
+    url(r'^author/(?P<author>[\w-]+)/$', 'list', name='author_archive'),
+    url(r'^confirm/comment/$', 'comment_posted', name='comment_posted'),
+    url(r'^post/(?P<slug>[\w-]+)/$', 'view_post', name='view_post'),
 )
--- src/cms/views.py
+++ src/cms/views.py
@@ -6,3 +6,7 @@ def index(request):
     """Index view"""
     context = {}
     return render(request, 'cms/index.html', context)
+
+
+def list(request, year=None, month=None, tag=None, author=None):
+    pass
